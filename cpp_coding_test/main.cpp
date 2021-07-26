dd34337d98e9f1796d418f88e093ccfb7ad7f9a1
NimbleV_Mac <viixiv@naver.com>
2021-07-26 23:22:45 +0900

    Initial Commit

---
+++ cpp_coding_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_coding_test
+//
+//  Created by appdori on 2021/07/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
