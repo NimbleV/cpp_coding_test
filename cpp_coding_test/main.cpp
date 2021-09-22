34e6f9e6a1ecbaf1ebfb3dbb7b688ca21fc609bb
NimbleV_Mac <viixiv@naver.com>
2021-09-22 14:02:45 +0900

    필요없는 일부 코드 지움, 함수정의 대신 람다[] 사용으로 변경, 일부 변수명 변경.

--- cpp_coding_test/main.cpp
+++ cpp_coding_test/main.cpp
@@ -7,44 +7,35 @@
 
 using namespace std;
 
-bool comp_by_second(pair<string, int> a, pair<string, int> b) {
-    return a.second > b.second;
-}
-
-bool comp_by_first(pair<int, int> a, pair<int, int> b) {
-    return a.first > b.first;
-}
-
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
 
-    unordered_map<string, int> um_genre_totalPlay; // map<genre, total_play>
-    unordered_map<string, vector<pair<int, int>>> um_arr; // map < genre, arr < pair<play, index> > >
+    unordered_map<string, int> map_genre_totalPlay; // map<genre, total_play>
+    unordered_map<string, vector<pair<int, int>>> map_genre_arr; // map < genre, arr < pair<play, index> > >
 
     for (int i=0; i<genres.size(); i++) {
         // 1. count total plays of each genres
         string genre = genres[i];
-        if (um_genre_totalPlay.find(genre) == um_genre_totalPlay.end()) { // can not find
-            um_genre_totalPlay[genre] = plays[i]; // init
-        } else {
-            um_genre_totalPlay[genre] += plays[i]; // add
-        }
+        map_genre_totalPlay[genre] += plays[i]; // add
         // 2. make arrays for each genres
-        um_arr[genre].push_back( make_pair(plays[i], i) ); // pair<play, index>
+        map_genre_arr[genre].push_back( make_pair(plays[i], i) ); // pair<play, index>
     }
     
     // 3. sort genres by totalPlay
-    vector<pair<string, int>> genre_array(um_genre_totalPlay.begin(), um_genre_totalPlay.end());
-    sort(genre_array.begin(), genre_array.end(), comp_by_second);
+    vector<pair<string, int>> genre_array(map_genre_totalPlay.begin(), map_genre_totalPlay.end());
+    sort(genre_array.begin(), genre_array.end(), [] (pair<string, int>& a, pair<string, int>& b) {
+        return a.second > b.second;
+    });
     
     // 4. get top two songs for each genre
     for (int i=0; i<genre_array.size(); i++) {
         string genre = genre_array[i].first;
-        
-        vector<pair<int, int>> arr_play_index = um_arr[genre];
+        vector<pair<int, int>> arr_play_index = map_genre_arr[genre];
         
         //sort
-        sort(arr_play_index.begin(), arr_play_index.end(), comp_by_first);
+        sort(arr_play_index.begin(), arr_play_index.end(), [] (pair<int, int>& a, pair<int, int>& b) {
+            return a.first> b.first;
+        });
         
         //push index to answer
         for (int j=0; j< min((int)arr_play_index.size(), 2); j++) { // max 2
