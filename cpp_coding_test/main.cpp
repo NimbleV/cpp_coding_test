ceba6f4d14a676ecd3248c028820772189c7b900
NimbleV_Mac <viixiv@naver.com>
2021-09-22 18:45:30 +0900

    "기능개발" 문제 제출한 정답.

--- cpp_coding_test/main.cpp
+++ cpp_coding_test/main.cpp
@@ -1,57 +1,61 @@
 #include <string>
 #include <vector>
-
-#include <unordered_map>
 #include <iostream>
-#include <algorithm>
+
+//기능개발
 
 using namespace std;
 
-vector<int> solution(vector<string> genres, vector<int> plays) {
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-
-    unordered_map<string, int> map_genre_totalPlay; // map<genre, total_play>
-    unordered_map<string, vector<pair<int, int>>> map_genre_arr; // map < genre, arr < pair<play, index> > >
-
-    for (int i=0; i<genres.size(); i++) {
-        // 1. count total plays of each genres
-        string genre = genres[i];
-        map_genre_totalPlay[genre] += plays[i]; // add
-        // 2. make arrays for each genres
-        map_genre_arr[genre].push_back( make_pair(plays[i], i) ); // pair<play, index>
+    
+    vector<int> remains(progresses.size());
+    for (int i=0; i<progresses.size(); i++) {
+        remains[i] = ((100-progresses[i]-1) / speeds[i]) + 1;
     }
+
+    int cur_value = -1;
+    int cur_len = 0;
     
-    // 3. sort genres by totalPlay
-    vector<pair<string, int>> genre_array(map_genre_totalPlay.begin(), map_genre_totalPlay.end());
-    sort(genre_array.begin(), genre_array.end(), [] (pair<string, int>& a, pair<string, int>& b) {
-        return a.second > b.second;
-    });
-    
-    // 4. get top two songs for each genre
-    for (int i=0; i<genre_array.size(); i++) {
-        string genre = genre_array[i].first;
-        vector<pair<int, int>> arr_play_index = map_genre_arr[genre];
-        
-        //sort
-        sort(arr_play_index.begin(), arr_play_index.end(), [] (pair<int, int>& a, pair<int, int>& b) {
-            return a.first> b.first;
-        });
-        
-        //push index to answer
-        for (int j=0; j< min((int)arr_play_index.size(), 2); j++) { // max 2
-            answer.push_back(arr_play_index[j].second);
+    for (int i=0; i<remains.size(); i++) {
+        if (cur_value < remains[i]) {
+            answer.push_back(cur_len);
+            cur_value = remains[i];
+            cur_len = 1;
+        }
+        else {
+            cur_len++;
         }
     }
+    answer.push_back(cur_len);
+    answer.erase(answer.begin());
     
     return answer;
 }
 
+
 int main() {
-    vector<string> genres = {"classic", "pop", "classic", "classic", "pop"};
-    vector<int> plays = {500, 600, 150, 800, 2500};
-    vector<int> ans = solution(genres, plays);
-    for(int a : ans) {
-        cout << a;
+    vector<int> progresses1 = {93, 30, 55};
+    vector<int> speeds1 = {1, 30, 5};
+    vector<int> ans1 = solution(progresses1, speeds1);
+    for(int a : ans1) {
+        cout << a; //[2, 1]
+    }
+    cout << endl;
+    
+    vector<int> progresses2 = {95, 90, 99, 99, 80, 99};
+    vector<int> speeds2 = {1, 1, 1, 1, 1, 1};
+    vector<int> ans2 = solution(progresses2, speeds2);
+    for(int a : ans2) {
+        cout << a; //[1, 3, 2]
+    }
+    cout << endl;
+    
+    vector<int> progresses3 = {99, 98, 97, 96, 95, 94};
+    vector<int> speeds3 = {1, 1, 1, 1, 1, 1};
+    vector<int> ans3 = solution(progresses3, speeds3);
+    for(int a : ans3) {
+        cout << a; //[1, 3, 2]
     }
     cout << endl;
     
