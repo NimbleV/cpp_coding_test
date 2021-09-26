59f5ba966c54487da7f07e21eba89a6ec56e3fc8
NimbleV_Mac <viixiv@naver.com>
2021-09-26 13:28:41 +0900

    "다리를 지나는 트럭" 정답.

--- cpp_coding_test/main.cpp
+++ cpp_coding_test/main.cpp
@@ -1,48 +1,59 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
+#include <queue>
 
-int solution(std::vector<int> priorities, int position) {
-    int ans = 1;
+using namespace std;
 
-    while (1) {
-        //find max
-        std::vector<int>::iterator iter = std::max_element(priorities.begin(), priorities.end());
-        //int max_val = *iter;
-        long max_pos = std::distance(priorities.begin(), iter);
-        
-        //move elements
-        for (int i = 0; i < max_pos; i++) {
-            priorities.push_back(priorities[0]); // copy back
-            priorities.erase(priorities.begin()); // remove front
-            position--;
-            if (position < 0) position = (int)priorities.size() - 1;
-        }
+//다리를 지나는 트럭
 
-        //check
-        if (position == 0) return ans;
-
-        //remove first element
-        priorities.erase(priorities.begin());
-        position--;
-        ans++;
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    
+    queue<int> bridge;
+    for (int i=0; i<bridge_length; i++) {
+        bridge.push(0);
     }
-
-    return 0;
+    int total_weight=0;
+    
+    for (int i=0; i<truck_weights.size(); ) {//여기서는 마지막 차를 다리에 올리는 것까지만 계산.
+        total_weight -= bridge.front();//다리를 건넌 차 무게 제거.
+        bridge.pop();
+        
+        if (total_weight + truck_weights[i] <= weight) {//무게가 허용되면 다리에 올린다.
+            total_weight += truck_weights[i];
+            bridge.push(truck_weights[i]);
+            i++;//다음 차 대기.
+        } else {
+            bridge.push(0);//차를 못 올리면. 0은 빈 칸.
+        }
+        
+        answer++;
+    }
+    
+    answer += bridge_length;//마지막 차가 다리를 지나는데 필요한 추가 시간.
+    
+    return answer;
 }
 
-
 int main() {
-    std::vector<int> priorities1 = {2, 1, 3, 2};
-    int location1 = 2;
-    int ans1 = solution(priorities1, location1);//1
-    std::cout << ans1 << std::endl;
+    int bridge_length = 2;
+    int weight = 10;
+    std::vector<int> truck_weights = {7, 4, 5, 6};
+    int ans = solution(bridge_length, weight, truck_weights);//8
+    std::cout << ans << std::endl;
     
-    std::vector<int> priorities2 = {1,1,9,1,1,1};
-    int location2 = 0;
-    int ans2 = solution(priorities2, location2);//5
+    int bridge_length2 = 100;
+    int weight2 = 100;
+    std::vector<int> truck_weights2 = {10};
+    int ans2 = solution(bridge_length2, weight2, truck_weights2);//10
     std::cout << ans2 << std::endl;
+    
+    int bridge_length3 = 100;
+    int weight3 = 100;
+    std::vector<int> truck_weights3 = {10,10,10,10,10,10,10,10,10,10};
+    int ans3 = solution(bridge_length3, weight3, truck_weights3);//11
+    std::cout << ans3 << std::endl;
     
     return 0;
 }
